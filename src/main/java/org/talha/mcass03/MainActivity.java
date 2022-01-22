35ae920f62780429f1e1b08639f4e0f8ff18fe0e
Mubashir338 <79444924+Mubashir338@users.noreply.github.com>
2022-01-22 21:02:51 +0500

    Manifest Updated

--- src/main/java/org/talha/mcass03/MainActivity.java
+++ src/main/java/org/talha/mcass03/MainActivity.java
@@ -3,13 +3,18 @@ package org.talha.mcass03;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +24,17 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView weatherRV;
     private TextInputEditText cityEdt;
     private ImageView backIV,iconIV,searchIV;
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE=1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         setContentView(R.layout.activity_main);
         homeRL = findViewById(R.id.idRLHome);
         loadingPB=findViewById(R.id.idPBLoading);
@@ -36,10 +47,17 @@ public class MainActivity extends AppCompatActivity {
         iconIV=findViewById(R.id.idIVIcon);
         searchIV=findViewById(R.id.idIVSearch);
 
+        weatherRVModalArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this, weatherRVModalArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
 
-
-
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
     }
+    private void getWeatherInfo(String cityName)
+    {
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=dca892fc2d534382a1461857211207&q="+ cityName +"&days=1&aqi=yes&alerts=yes";
+
+    }
+
 }
