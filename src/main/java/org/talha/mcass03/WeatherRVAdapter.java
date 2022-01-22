24bc20b372ee464bc245c3c941e278a8ed5d0cc5
Mubashir338 <79444924+Mubashir338@users.noreply.github.com>
2022-01-22 20:38:57 +0500

    Adapter Updated

--- src/main/java/org/talha/mcass03/WeatherRVAdapter.java
+++ src/main/java/org/talha/mcass03/WeatherRVAdapter.java
@@ -1,11 +1,16 @@
 package org.talha.mcass03;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,22 +26,34 @@ public class WeatherRVAdapter extends RecyclerView.Adapter <WeatherRVAdapter.Vie
     @NonNull
     @Override
     public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
+        WeatherRVModal modal = weatherRVModalArrayList.get(position);
+        holder.temperatureTV.setText(modal.getTemperature());
+        Picasso.get().load("http:".concat(modal.getIcon())).into(holder.conditionTV);
+        holder.windTV.setText(modal.getWindSpeed()+"Km/h");
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return weatherRVModalArrayList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView windTV, temperatureTV, timeTV;
+        private ImageView conditionTV;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            windTV = itemView.findViewById(R.id.idTVWindSpeed);
+            temperatureTV = itemView.findViewById(R.id.idTVTemperature);
+            timeTV = itemView.findViewById(R.id.idTVTime);
+            conditionTV = itemView.findViewById(R.id.idTVCondition);
         }
     }
 }
