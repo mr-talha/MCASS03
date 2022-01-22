eda082c752c91efcdf345a2e387c4f1499ea2812
Mubashir338 <79444924+Mubashir338@users.noreply.github.com>
2022-01-22 19:13:18 +0500

    Adapter Class working

--- src/main/java/org/talha/mcass03/WeatherRVAdapter.java
+++ src/main/java/org/talha/mcass03/WeatherRVAdapter.java
@@ -1,4 +1,42 @@
 package org.talha.mcass03;
 
-public class WeatherRVAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter <WeatherRVAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVModal> weatherRVModalArrayList) {
+        this.context = context;
+        this.weatherRVModalArrayList = weatherRVModalArrayList;
+    }
+
+    @NonNull
+    @Override
+    public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
